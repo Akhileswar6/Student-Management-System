f423c464ddf0a50734966759faaddbeaaba838c7
Akhileswar6 <akhilkamale@gmail.com>
2025-02-15 17:21:41 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-const port = 3012
+const port = .3012
 
 const app = express()
 app.use(express.static(__dirname))
